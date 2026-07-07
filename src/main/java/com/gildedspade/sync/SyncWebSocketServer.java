95f4efd5b0b446b070afe6f828f9b87c6b87310a
Mars <m4rsbars@gmail.com>
2026-07-07 08:22:59 +0100

    Switched 3rd party java websocket for okhttp

--- src/main/java/com/gildedspade/sync/SyncWebSocketServer.java
+++ src/main/java/com/gildedspade/sync/SyncWebSocketServer.java
@@ -4,22 +4,39 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.callback.ClientThread;
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okhttp3.internal.ws.RealWebSocket;
+import okhttp3.internal.ws.WebSocketProtocol;
+import okio.BufferedSink;
+import okio.BufferedSource;
+import okio.Okio;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 @Slf4j
-public class SyncWebSocketServer extends WebSocketServer
+public class SyncWebSocketServer
 {
 	private final GildedSpadeSyncPlugin plugin;
 	private final ClientThread clientThread;
@@ -27,15 +44,17 @@ public class SyncWebSocketServer extends WebSocketServer
 	private final int port;
 	private final AtomicBoolean serverActive = new AtomicBoolean(false);
 	private final AtomicBoolean startupFailed = new AtomicBoolean(false);
+	private final Set<Connection> connections = ConcurrentHashMap.newKeySet();
+
+	private ServerSocket serverSocket;
+	private Thread acceptThread;
 
 	public SyncWebSocketServer(int port, GildedSpadeSyncPlugin plugin, ClientThread clientThread, Gson gson)
 	{
-		super(new InetSocketAddress("localhost", port));
 		this.plugin = plugin;
 		this.clientThread = clientThread;
 		this.gson = Objects.requireNonNull(gson);
 		this.port = port;
-		setReuseAddr(true);
 	}
 
 	public int getPort()
@@ -55,37 +74,103 @@ public class SyncWebSocketServer extends WebSocketServer
 
 	public int getConnectionCount()
 	{
-		return getConnections().size();
+		return connections.size();
 	}
 
-	@Override
-	public void onOpen(WebSocket conn, ClientHandshake handshake)
+	public void start() throws IOException
 	{
-		if (getConnections().size() > 1)
+		ServerSocket socket = new ServerSocket();
+		socket.setReuseAddress(true);
+		socket.bind(new InetSocketAddress(InetAddress.getLoopbackAddress(), port));
+
+		serverSocket = socket;
+		startupFailed.set(false);
+		serverActive.set(true);
+
+		acceptThread = new Thread(this::acceptLoop, "gilded-spade-sync-ws-" + port);
+		acceptThread.setDaemon(true);
+		acceptThread.start();
+
+		log.info("WebSocket server started successfully");
+	}
+
+	public void stop(int timeoutMillis) throws InterruptedException
+	{
+		serverActive.set(false);
+		closeQuietly(serverSocket);
+		for (Connection connection : new ArrayList<>(connections))
 		{
-			log.warn("Connection limit reached, rejecting {}", conn.getRemoteSocketAddress());
-			conn.close(1008, "Connection limit reached");
-			return;
+			connection.close(1001, "Server stopping");
+			connection.closeQuietly();
 		}
-		log.info("New connection from: {}", conn.getRemoteSocketAddress());
+		connections.clear();
+
+		Thread thread = acceptThread;
+		if (thread != null)
+		{
+			thread.join(timeoutMillis);
+		}
+	}
+
+	private void acceptLoop()
+	{
+		while (serverActive.get())
+		{
+			try
+			{
+				Socket socket = serverSocket.accept();
+				if (connections.size() >= 1)
+				{
+					rejectConnection(socket, 503, "Connection limit reached");
+					continue;
+				}
+
+				Connection connection = new Connection(socket);
+				connections.add(connection);
+
+				Thread connectionThread = new Thread(() -> handleConnection(connection),
+					"gilded-spade-sync-ws-client-" + port);
+				connectionThread.setDaemon(true);
+				connectionThread.start();
+			}
+			catch (SocketException e)
+			{
+				if (serverActive.get())
+				{
+					log.error("WebSocket accept loop failed", e);
+					startupFailed.set(true);
+					serverActive.set(false);
+				}
+			}
+			catch (Exception e)
+			{
+				log.error("WebSocket accept loop failed", e);
+			}
+		}
+	}
+
+	private void handleConnection(Connection connection)
+	{
 		try
 		{
-			broadcastUsername();
+			connection.handshake();
+			connection.loopReader();
 		}
 		catch (Exception e)
 		{
-			log.error("Error broadcasting username on connection open", e);
+			if (serverActive.get())
+			{
+				log.debug("WebSocket connection ended for {}", connection.getRemoteSocketAddress(), e);
+			}
+		}
+		finally
+		{
+			connections.remove(connection);
+			connection.closeQuietly();
 		}
 	}
 
-	@Override
-	public void onClose(WebSocket conn, int code, String reason, boolean remote)
-	{
-		log.info("Connection closed: {} - Reason: {}", conn.getRemoteSocketAddress(), reason);
-	}
-
-	@Override
-	public void onMessage(WebSocket conn, String message)
+	private void handleMessage(Connection conn, String message)
 	{
 		try
 		{
@@ -190,7 +275,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		}
 	}
 
-	private <T> void handleRequest(WebSocket conn, int sequenceId, String responseType,
+	private <T> void handleRequest(Connection conn, int sequenceId, String responseType,
 									String logName, Supplier<T> dataSupplier)
 	{
 		clientThread.invoke(() ->
@@ -201,8 +286,8 @@ public class SyncWebSocketServer extends WebSocketServer
 				response.put("type", responseType);
 				response.put("sequenceId", sequenceId);
 				response.put("data", dataSupplier.get());
-			conn.send(gson.toJson(response));
-			log.debug("Sent {} to {}", logName, conn.getRemoteSocketAddress());
+				conn.send(gson.toJson(response));
+				log.debug("Sent {} to {}", logName, conn.getRemoteSocketAddress());
 			}
 			catch (Exception e)
 			{
@@ -212,7 +297,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		});
 	}
 
-	private void handleSortAction(WebSocket conn, int sequenceId, String logName, Consumer<Runnable> action)
+	private void handleSortAction(Connection conn, int sequenceId, String logName, Consumer<Runnable> action)
 	{
 		try
 		{
@@ -229,7 +314,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		}
 	}
 
-	private void handleSetBankFilter(WebSocket conn, int sequenceId, JsonObject request)
+	private void handleSetBankFilter(Connection conn, int sequenceId, JsonObject request)
 	{
 		try
 		{
@@ -259,7 +344,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		}
 	}
 
-	private void handleStartBankSortAssist(WebSocket conn, int sequenceId, JsonObject request)
+	private void handleStartBankSortAssist(Connection conn, int sequenceId, JsonObject request)
 	{
 		try
 		{
@@ -291,7 +376,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		}
 	}
 
-	private void sendBankSortAssistStatus(WebSocket conn, int sequenceId)
+	private void sendBankSortAssistStatus(Connection conn, int sequenceId)
 	{
 		Map<String, Object> response = new HashMap<>();
 		response.put("type", "BANK_SORT_ASSIST_STATUS");
@@ -300,7 +385,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		conn.send(gson.toJson(response));
 	}
 
-	private void handlePing(WebSocket conn, int sequenceId)
+	private void handlePing(Connection conn, int sequenceId)
 	{
 		Map<String, Object> response = new HashMap<>();
 		response.put("type", "PONG");
@@ -308,7 +393,7 @@ public class SyncWebSocketServer extends WebSocketServer
 		conn.send(gson.toJson(response));
 	}
 
-	private void sendError(WebSocket conn, int sequenceId, String errorMessage)
+	private void sendError(Connection conn, int sequenceId, String errorMessage)
 	{
 		Map<String, Object> response = new HashMap<>();
 		response.put("type", "ERROR");
@@ -367,8 +452,8 @@ public class SyncWebSocketServer extends WebSocketServer
 				Map<String, Object> broadcast = new HashMap<>();
 				broadcast.put("type", "PLAYER_UPDATE");
 				broadcast.put("data", plugin.getAllPlayerData());
-			broadcast(gson.toJson(broadcast));
-			log.debug("Broadcast player data update");
+				broadcast(gson.toJson(broadcast));
+				log.debug("Broadcast player data update");
 			}
 			catch (Exception e)
 			{
@@ -377,26 +462,210 @@ public class SyncWebSocketServer extends WebSocketServer
 		});
 	}
 
-	@Override
-	public void onError(WebSocket conn, Exception ex)
+	private void broadcast(String message)
 	{
-		if (conn != null)
+		for (Connection connection : connections)
 		{
-			log.error("WebSocket error for connection: {}", conn.getRemoteSocketAddress(), ex);
-		}
-		else
-		{
-			log.error("WebSocket server error", ex);
-			startupFailed.set(true);
-			serverActive.set(false);
+			connection.send(message);
 		}
 	}
 
-	@Override
-	public void onStart()
+	private void rejectConnection(Socket socket, int statusCode, String message)
 	{
-		log.info("WebSocket server started successfully");
-		startupFailed.set(false);
-		serverActive.set(true);
+		try (Socket ignored = socket;
+			BufferedSink sink = Okio.buffer(Okio.sink(socket)))
+		{
+			sink.writeUtf8("HTTP/1.1 " + statusCode + " " + message + "\r\n");
+			sink.writeUtf8("Connection: close\r\n");
+			sink.writeUtf8("Content-Length: 0\r\n\r\n");
+			sink.flush();
+		}
+		catch (IOException e)
+		{
+			log.debug("Unable to reject extra WebSocket connection", e);
+		}
+	}
+
+	private static String headerValue(Map<String, String> headers, String name)
+	{
+		return headers.get(name.toLowerCase(Locale.ROOT));
+	}
+
+	private static boolean headerContains(Map<String, String> headers, String name, String expectedValue)
+	{
+		String value = headerValue(headers, name);
+		return value != null && value.toLowerCase(Locale.ROOT).contains(expectedValue.toLowerCase(Locale.ROOT));
+	}
+
+	private static void closeQuietly(Closeable closeable)
+	{
+		if (closeable == null)
+		{
+			return;
+		}
+		try
+		{
+			closeable.close();
+		}
+		catch (IOException ignored)
+		{
+			// Best-effort shutdown.
+		}
+	}
+
+	private class Connection extends WebSocketListener
+	{
+		private final Socket socket;
+		private RealWebSocket webSocket;
+		private BufferedSource source;
+		private BufferedSink sink;
+
+		Connection(Socket socket)
+		{
+			this.socket = socket;
+		}
+
+		void handshake() throws IOException
+		{
+			socket.setSoTimeout(5000);
+			source = Okio.buffer(Okio.source(socket));
+			sink = Okio.buffer(Okio.sink(socket));
+
+			String requestLine = source.readUtf8LineStrict();
+			String[] requestParts = requestLine.split(" ", 3);
+			if (requestParts.length < 2 || !"GET".equals(requestParts[0]))
+			{
+				throw new IOException("Invalid WebSocket handshake request");
+			}
+
+			Map<String, String> headers = new HashMap<>();
+			String line;
+			while ((line = source.readUtf8LineStrict()).length() > 0)
+			{
+				int separator = line.indexOf(':');
+				if (separator > 0)
+				{
+					headers.put(line.substring(0, separator).trim().toLowerCase(Locale.ROOT),
+						line.substring(separator + 1).trim());
+				}
+			}
+
+			String key = headerValue(headers, "Sec-WebSocket-Key");
+			if (!headerContains(headers, "Upgrade", "websocket")
+				|| !headerContains(headers, "Connection", "Upgrade")
+				|| key == null
+				|| !"13".equals(headerValue(headers, "Sec-WebSocket-Version")))
+			{
+				throw new IOException("Invalid WebSocket upgrade headers");
+			}
+
+			sink.writeUtf8("HTTP/1.1 101 Switching Protocols\r\n");
+			sink.writeUtf8("Upgrade: websocket\r\n");
+			sink.writeUtf8("Connection: Upgrade\r\n");
+			sink.writeUtf8("Sec-WebSocket-Accept: " + WebSocketProtocol.acceptHeader(key) + "\r\n\r\n");
+			sink.flush();
+
+			String path = requestParts[1].startsWith("/") ? requestParts[1] : "/";
+			Request request = new Request.Builder()
+				.url("ws://localhost:" + port + path)
+				.build();
+
+			webSocket = new RealWebSocket(request, this, new Random(), 0);
+			RealWebSocket.Streams streams = new RealWebSocket.Streams(false, source, sink)
+			{
+				@Override
+				public void close() throws IOException
+				{
+					socket.close();
+				}
+			};
+			webSocket.initReaderAndWriter("Gilded Spade Sync", streams);
+			socket.setSoTimeout(0);
+			onOpen(webSocket, response(request));
+		}
+
+		void loopReader() throws IOException
+		{
+			webSocket.loopReader();
+		}
+
+		void send(String message)
+		{
+			WebSocket currentWebSocket = webSocket;
+			if (currentWebSocket != null)
+			{
+				currentWebSocket.send(message);
+			}
+		}
+
+		void close(int code, String reason)
+		{
+			WebSocket currentWebSocket = webSocket;
+			if (currentWebSocket != null)
+			{
+				currentWebSocket.close(code, reason);
+			}
+		}
+
+		void closeQuietly()
+		{
+			SyncWebSocketServer.closeQuietly(socket);
+		}
+
+		String getRemoteSocketAddress()
+		{
+			return String.valueOf(socket.getRemoteSocketAddress());
+		}
+
+		@Override
+		public void onOpen(WebSocket webSocket, Response response)
+		{
+			log.info("New connection from: {}", getRemoteSocketAddress());
+			try
+			{
+				broadcastUsername();
+			}
+			catch (Exception e)
+			{
+				log.error("Error broadcasting username on connection open", e);
+			}
+		}
+
+		@Override
+		public void onMessage(WebSocket webSocket, String text)
+		{
+			handleMessage(this, text);
+		}
+
+		@Override
+		public void onClosing(WebSocket webSocket, int code, String reason)
+		{
+			webSocket.close(code, reason);
+		}
+
+		@Override
+		public void onClosed(WebSocket webSocket, int code, String reason)
+		{
+			log.info("Connection closed: {} - Reason: {}", getRemoteSocketAddress(), reason);
+		}
+
+		@Override
+		public void onFailure(WebSocket webSocket, Throwable t, Response response)
+		{
+			if (serverActive.get())
+			{
+				log.error("WebSocket error for connection: {}", getRemoteSocketAddress(), t);
+			}
+		}
+
+		private Response response(Request request)
+		{
+			return new Response.Builder()
+				.request(request)
+				.protocol(Protocol.HTTP_1_1)
+				.code(101)
+				.message("Switching Protocols")
+				.build();
+		}
 	}
 }
